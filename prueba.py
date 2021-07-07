4dbb0225698445972903eefac447fcbf76056b86
Luiggy Macias <ferrinluiggy@gmail.com>
2021-07-07 13:55:47 -0500

    updated

--- prueba.py
+++ prueba.py
@@ -1,7 +1,14 @@
 #serverfix fajkajhdjslajajja
 
 def addTwoNumbers(a, b):
-	return a + b;
+	return a + b
 
 print(addTwoNumbers(2,3))
 #comment made in remotebranch
+
+#filter an array of elements
+def filter():
+	print('hola mi compa')
+	return 2+2
+
+print(filter())
