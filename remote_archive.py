12fb2a4a8f79d58abe7285d370e2c00c536f2fe7
Luiggy Macias <ferrinluiggy@gmail.com>
2021-07-30 08:14:40 -0500

    djd

--- remote_archive.py
+++ remote_archive.py
@@ -1,3 +1,10 @@
 # this file was created in serverfix remote branch
 for i in range(3):
   print(i)
+
+def esPar(num):
+  if num % 2 == 0:
+    print('es par')
+  else:
+    print('no es par')
+esPar(223)
