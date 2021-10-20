b4af7f85be03c8e356da9a87ebfe24b8227c1c40
macluiggy <ferrinluiggy@gmail.com>
2021-10-20 07:47:34 -0500

    ehehhe

---
+++ prueba.py
@@ -0,0 +1,23 @@
+#serverfix fajkajhdjslajajja
+
+def addTwoNumbers(a, b):
+	return a + b
+
+print(addTwoNumbers(2,3))
+#comment made in remotebranch
+
+#filter an array of elements
+def filter():
+	print('hola mi compa')
+	return 2+2
+
+print(filter())
+
+#c3 rebase jajja
+#cambio --patch
+
+print(len('Capitalized, short (50 chars or less) summary'))
+#cambio en la rama main
+#cambio en la rama main
+#hola
+#...d
